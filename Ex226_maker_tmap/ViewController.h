821709317d3d0eddf13dd0f824b6179f12d837b7
saerem21 <nemessisdds>
2014-01-17 13:34:24 +0900

    Initial Commit

---
+++ Ex226_maker_tmap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex226_maker_tmap
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ex226_maker_tmapTests/Ex226_maker_tmapTests.m
@@ -0,0 +1,34 @@
+//
+//  Ex226_maker_tmapTests.m
+//  Ex226_maker_tmapTests
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex226_maker_tmapTests : XCTestCase
+
+@end
+
+@implementation Ex226_maker_tmapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
